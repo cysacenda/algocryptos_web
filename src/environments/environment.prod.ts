eda015b3b06d1f527f7a01243e401b636d83f424
cysacenda <cysacenda@gmail.com>
2018-02-06 19:27:15 +0100

    change api location to aws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api/',
+  apiUrl: 'https://d34xbsgnyo3jm0.cloudfront.net/api/',
   coinsUrl: 'coins',
   processesInfosUrl: 'processes'
 };
