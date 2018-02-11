dbd26c5f450b632b0e6ffbbfd6bad2b43646f4a4
cysacenda <cysacenda@gmail.com>
2018-02-11 18:08:56 +0100

    homogenize design

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/',
+   apiUrl: 'http://localhost:3000/api/',
+  /*apiUrl: 'https://d34xbsgnyo3jm0.cloudfront.net/api/',*/
   coinsUrl: 'coins',
   processesInfosUrl: 'processes',
   missingSocialUrl: 'missingCoins'
