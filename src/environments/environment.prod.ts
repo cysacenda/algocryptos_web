1e633d5cdf8df7cd29679849c00d06428db1685f
cysacenda <cysacenda@gmail.com>
2018-02-15 21:08:43 +0100

    homogenize design

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://d34xbsgnyo3jm0.cloudfront.net/api/',
   coinsUrl: 'coins',
-  processesInfosUrl: 'processes'
+  processesInfosUrl: 'processes',
+  missingSocialUrl: 'missingCoins'
 };
